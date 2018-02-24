5067d3075905e1d0fdb46f39504731a82c0ae1c1
zmahfuz817 <zunayed.lm@gmail.com>
2018-02-24 02:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays(){
 function happyHolidaysTo(bob){
   return `Happy holidays, ${bob}!`
 }
-function happyHolidaysTo(Happy Independence Day, Lauren) {
+function happyHolidaysTo(holiday, name) {
   return `Happy ${Independence Day}, ${Lauren}!``
 
 }
